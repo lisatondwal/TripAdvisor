9ee83f9cf4912f43a0bf0fb452bd63434aa6b3be
lisatondwal <lisatondwal3792@gmail.com>
2022-12-26 17:01:18 +0530

    changes
    please update with pages
    
    Co-Authored-By: RohidasSkadam <119380481+rohidasSkadam@users.noreply.github.com>

---
+++ src/main/java/pageLayer/LoginPageLayer.java
@@ -0,0 +1,8 @@
+package pageLayer;
+
+public class LoginPageLayer {
+   public static void main(String arg[])
+   {
+	System.out.println("Login page");
+}
+}
---
+++ src/test/java/testLayer/LoginTestLayer.java
@@ -0,0 +1,12 @@
+package testLayer;
+
+import org.testng.annotations.Test;
+
+public class LoginTestLayer {
+	
+	public static void main(String arg[])
+	   {
+		System.out.println("Login test");
+	}
+
+}
